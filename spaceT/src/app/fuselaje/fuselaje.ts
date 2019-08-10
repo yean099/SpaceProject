d718c9ca09ee84c48ad16b62f95e5c65e808041f
esteban <yean099@gmail.com>
2019-08-10 01:04:15 -0500

    finalizada prueba con los requerimientos

--- spaceT/src/app/fuselaje/fuselaje.ts
+++ spaceT/src/app/fuselaje/fuselaje.ts
@@ -17,6 +17,7 @@ export class fuselaje implements OnInit {
     image: string;
     degrees: string;
     planet: string;
+    planets= ['mercurio','venus','tierra','marte','jupiter','saturno','urano','neptuno','pluton'];
 
   
     constructor(private _decimalPipe: DecimalPipe, public json: PlanetasService, public rutaActiva: ActivatedRoute, private router: Router){
@@ -28,19 +29,23 @@ export class fuselaje implements OnInit {
 
             this.pathname = window.location.pathname;
             let str1: string = this.pathname;
-            let str2: string = '/';
-            let resp= str1.split(str2);
-
-
-            console.log(resp);
-            this.json.getPlanetas('http://localhost:3000'+this.pathname).subscribe((res: any)=> {
-               
+            let resp= str1.split('/');
+    
+        if(resp.includes('planetas') && this.planets.includes(resp[resp.length-1]))
+        {
+                this.json.getPlanetas('http://localhost:3000'+this.pathname).subscribe((res: any)=> {
                 
-                this.image="'"+res['img']+"'";
-                this.degrees=res['degrees']+".0000";  
-                console.log(this.degrees);
-        });      
-        
+                    this.planet=resp[resp.length-1];
+                    this.image="'"+res['img']+"'";
+                    this.degrees=res['degrees']+".0000";  
+                    console.log(this.degrees);
+            });      
+
+         }
+         else{
+            this.planet='x';
+            this.degrees='y';
+         }
     }
 
     getUrl()
